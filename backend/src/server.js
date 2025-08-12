07d32ff30c6acc5f6cc5b04ddde7c831d5c197db
yildizaydnn <yildizaydd@gmail.com>
2025-08-12 21:51:51 +0300

    start server func added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,17 @@ import { connectDB } from "./config/db.js";
 
 const app = express();
 
-connectDB();
-
 app.get("/", (req, res) => res.send("hello from the server"));
-app.listen(ENV.PORT, () => console.log("server is running on port:", ENV.PORT));
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () =>
+      console.log("server is running on port:", ENV.PORT)
+    );
+  } catch (error) {
+    console.error("Error starting the server:", error.message);
+    process.exit(1); // Exit the process with a failure code
+  }
+};
+startServer();
